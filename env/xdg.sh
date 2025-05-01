08a0331b469fa34a84de10caedf03dc3fa221eb1
IronBatman2715 <72224765+IronBatman2715@users.noreply.github.com>
2025-05-01 13:42:08 -0400

    Add XDG compliant env vars for programs I use

--- env/xdg.sh
+++ env/xdg.sh
@@ -22,3 +22,38 @@ export VIMINIT='let $MYVIMRC = !has("nvim") ? "$XDG_CONFIG_HOME/vim/vimrc" : "$X
 
 # wget
 export WGETRC="$XDG_CONFIG_HOME/wgetrc"
+
+## Development programs
+
+export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
+export PYTHON_HISTORY="$XDG_STATE_HOME/python_history"
+
+# node/npm
+export NODE_REPL_HISTORY="$XDG_DATA_HOME/node_repl_history"
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
+# node/npm end
+
+# pnpm
+export PNPM_HOME="$XDG_DATA_HOME/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
+
+# rust
+export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
+export CARGO_HOME="$XDG_DATA_HOME/cargo"
+if [ -e "$CARGO_HOME/env" ]; then . "$CARGO_HOME/env"; fi
+# rust end
+
+# juliaup
+export JULIA_DEPOT_PATH="$XDG_DATA_HOME/julia:$JULIA_DEPOT_PATH"
+export JULIAUP_DEPOT_PATH="$XDG_DATA_HOME/julia"
+
+# TODO: not a spec, manually moved directory and edited this block
+case ":$PATH:" in
+  *":$XDG_DATA_HOME/juliaup/bin:"*) ;;
+  *) export PATH="$XDG_DATA_HOME/juliaup/bin${PATH:+:${PATH}}" ;;
+esac
+# juliaup end
