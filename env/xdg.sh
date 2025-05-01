51860cdf11d6a4f3a61fbf788b6078c647a028e7
IronBatman2715 <72224765+IronBatman2715@users.noreply.github.com>
2025-05-01 01:20:30 -0400

    Add comments and wgetrc to XDG setup

--- env/xdg.sh
+++ env/xdg.sh
@@ -7,10 +7,18 @@ export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_STATE_HOME="$HOME/.local/state"
 export XDG_BIN_HOME="$HOME/.local/bin" # not an official variable, but used by some programs
 
+## Common programs
+
+# bash
 export HISTFILE="$XDG_STATE_HOME/bash/history"
+
+# starship
 export STARSHIP_CONFIG="$XDG_CONFIG_HOME/starship.toml"
 export STARSHIP_CACHE="$XDG_CACHE_HOME/starship"
 
-# Required for vim <9.1.0327
+# vim: required for vim <9.1.0327
 export GVIMINIT='let $MYGVIMRC = !has("nvim") ? "$XDG_CONFIG_HOME/vim/gvimrc" : "$XDG_CONFIG_HOME/nvim/init.gvim" | so $MYGVIMRC'
 export VIMINIT='let $MYVIMRC = !has("nvim") ? "$XDG_CONFIG_HOME/vim/vimrc" : "$XDG_CONFIG_HOME/nvim/init.vim" | so $MYVIMRC'
+
+# wget
+export WGETRC="$XDG_CONFIG_HOME/wgetrc"
